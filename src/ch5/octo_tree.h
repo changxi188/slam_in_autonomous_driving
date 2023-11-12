bad26b44a5743fed8df457ebb2e06f8e6006c8fd
贝叶斯-成昌喜 <chengchangxi@holomatic.com>
2023-11-12 23:33:41 +0800

    Add: format

--- src/ch5/octo_tree.h
+++ src/ch5/octo_tree.h
@@ -12,31 +12,40 @@
 #include <map>
 #include <queue>
 
-namespace sad {
-
+namespace sad
+{
 // 3D Box 记录各轴上的最大最小值
-struct Box3D {
+struct Box3D
+{
     Box3D() = default;
     Box3D(float min_x, float max_x, float min_y, float max_y, float min_z, float max_z)
-        : min_{min_x, min_y, min_z}, max_{max_x, max_y, max_z} {}
+      : min_{min_x, min_y, min_z}, max_{max_x, max_y, max_z}
+    {
+    }
 
     /// 判断pt是否在内部
-    bool Inside(const Vec3f& pt) const {
+    bool Inside(const Vec3f& pt) const
+    {
         return pt[0] <= max_[0] && pt[0] >= min_[0] && pt[1] <= max_[1] && pt[1] >= min_[1] && pt[2] <= max_[2] &&
                pt[2] >= min_[2];
     }
 
     /// 点到3D Box距离
     /// 我们取外侧点到边界的最大值
-    float Dis(const Vec3f& pt) const {
+    float Dis(const Vec3f& pt) const
+    {
         float ret = 0;
-        for (int i = 0; i < 3; ++i) {
-            if (pt[i] < min_[i]) {
+        for (int i = 0; i < 3; ++i)
+        {
+            if (pt[i] < min_[i])
+            {
                 float d = min_[i] - pt[i];
-                ret = d > ret ? d : ret;
-            } else if (pt[i] > max_[i]) {
+                ret     = d > ret ? d : ret;
+            }
+            else if (pt[i] > max_[i])
+            {
                 float d = pt[i] - max_[i];
-                ret = d > ret ? d : ret;
+                ret     = d > ret ? d : ret;
             }
         }
 
@@ -49,15 +58,19 @@ struct Box3D {
 };
 
 /// octo tree 节点
-struct OctoTreeNode {
-    int id_ = -1;
-    int point_idx_ = -1;                    // 点的索引，-1为无效
-    Box3D box_;                             // 边界框
+struct OctoTreeNode
+{
+    int           id_        = -1;
+    int           point_idx_ = -1;          // 点的索引，-1为无效
+    Box3D         box_;                     // 边界框
     OctoTreeNode* children[8] = {nullptr};  // 子节点
 
-    bool IsLeaf() const {
-        for (const OctoTreeNode* n : children) {
-            if (n != nullptr) {
+    bool IsLeaf() const
+    {
+        for (const OctoTreeNode* n : children)
+        {
+            if (n != nullptr)
+            {
                 return false;
             }
         }
@@ -66,18 +79,28 @@ struct OctoTreeNode {
 };
 
 /// 用于记录knn结果
-struct NodeAndDistanceOcto {
-    NodeAndDistanceOcto(OctoTreeNode* node, float dis2) : node_(node), distance_(dis2) {}
-    OctoTreeNode* node_ = nullptr;
-    float distance_ = 0;  // 平方距离，用于比较
+struct NodeAndDistanceOcto
+{
+    NodeAndDistanceOcto(OctoTreeNode* node, float dis2) : node_(node), distance_(dis2)
+    {
+    }
+    OctoTreeNode* node_     = nullptr;
+    float         distance_ = 0;  // 平方距离，用于比较
 
-    bool operator<(const NodeAndDistanceOcto& other) const { return distance_ < other.distance_; }
+    bool operator<(const NodeAndDistanceOcto& other) const
+    {
+        return distance_ < other.distance_;
+    }
 };
 
-class OctoTree {
-   public:
+class OctoTree
+{
+public:
     explicit OctoTree() = default;
-    ~OctoTree() { Clear(); }
+    ~OctoTree()
+    {
+        Clear();
+    }
 
     bool BuildTree(const CloudPtr& cloud);
 
@@ -88,18 +111,22 @@ class OctoTree {
     bool GetClosestPointMT(const CloudPtr& cloud, std::vector<std::pair<size_t, size_t>>& match, int k = 5);
 
     /// 设置近似最近邻参数
-    void SetApproximate(bool use_ann = true, float alpha = 0.1) {
+    void SetApproximate(bool use_ann = true, float alpha = 0.1)
+    {
         approximate_ = use_ann;
-        alpha_ = alpha;
+        alpha_       = alpha;
     }
 
     /// 返回节点数量
-    size_t size() const { return size_; }
+    size_t size() const
+    {
+        return size_;
+    }
 
     /// 清理数据
     void Clear();
 
-   private:
+private:
     /// kdtree 构建相关
     /**
      * 在node处插入点
@@ -122,7 +149,10 @@ class OctoTree {
     void Reset();
 
     /// 两个点的平方距离
-    static inline float Dis2(const Vec3f& p1, const Vec3f& p2) { return (p1 - p2).squaredNorm(); }
+    static inline float Dis2(const Vec3f& p1, const Vec3f& p2)
+    {
+        return (p1 - p2).squaredNorm();
+    }
 
     // Knn 相关
     /**
@@ -147,16 +177,16 @@ class OctoTree {
      */
     bool NeedExpand(const Vec3f& pt, OctoTreeNode* node, std::priority_queue<NodeAndDistanceOcto>& knn_result) const;
 
-    int k_ = 5;                                     // knn最近邻数量
-    std::shared_ptr<OctoTreeNode> root_ = nullptr;  // 叶子节点
-    std::vector<Vec3f> cloud_;                      // 输入点云
-    std::map<int, OctoTreeNode*> nodes_;            // for bookkeeping
-    size_t size_ = 0;                               // 叶子节点数量
-    int tree_node_id_ = 0;                          // 为kdtree node 分配id
+    int                           k_    = 5;          // knn最近邻数量
+    std::shared_ptr<OctoTreeNode> root_ = nullptr;    // 叶子节点
+    std::vector<Vec3f>            cloud_;             // 输入点云
+    std::map<int, OctoTreeNode*>  nodes_;             // for bookkeeping
+    size_t                        size_         = 0;  // 叶子节点数量
+    int                           tree_node_id_ = 0;  // 为kdtree node 分配id
 
     // flann
-    bool approximate_ = false;
-    float alpha_ = 1.0;  // flann的距离倍数
+    bool  approximate_ = false;
+    float alpha_       = 1.0;  // flann的距离倍数
 };
 
 }  // namespace sad
