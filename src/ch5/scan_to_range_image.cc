933c7de1b9693d9e2cf407593ece1597004a5aa5
贝叶斯-成昌喜 <chengchangxi@holomatic.com>
2023-11-27 10:33:49 +0800

    Add: format

--- src/ch5/scan_to_range_image.cc
+++ src/ch5/scan_to_range_image.cc
@@ -10,7 +10,7 @@
 
 #include <opencv2/opencv.hpp>
 
-using PointType = pcl::PointXYZI;
+using PointType      = pcl::PointXYZI;
 using PointCloudType = pcl::PointCloud<PointType>;
 
 DEFINE_string(pcd_path, "./data/ch5/scan_example.pcd", "点云文件路径");
@@ -19,7 +19,8 @@ DEFINE_int32(elevation_rows, 16, "俯仰角对应的行数");
 DEFINE_double(elevation_range, 15.0, "俯仰角范围");  // VLP-16 上下各15度范围
 DEFINE_double(lidar_height, 1.128, "雷达安装高度");
 
-void GenerateRangeImage(PointCloudType::Ptr cloud) {
+void GenerateRangeImage(PointCloudType::Ptr cloud)
+{
     int image_cols = int(360 / FLAGS_azimuth_resolution_deg);  // 水平为360度，按分辨率切分即可
     int image_rows = FLAGS_elevation_rows;                     // 图像行数固定
     LOG(INFO) << "range image: " << image_rows << "x" << image_cols;
@@ -29,20 +30,23 @@ void GenerateRangeImage(PointCloudType::Ptr cloud) {
 
     double ele_resolution = FLAGS_elevation_range * 2 / FLAGS_elevation_rows;  // elevation分辨率
 
-    for (const auto& pt : cloud->points) {
-        double azimuth = atan2(pt.y, pt.x) * 180 / M_PI;
-        double range = sqrt(pt.x * pt.x + pt.y * pt.y);
+    for (const auto& pt : cloud->points)
+    {
+        double azimuth   = atan2(pt.y, pt.x) * 180 / M_PI;
+        double range     = sqrt(pt.x * pt.x + pt.y * pt.y);
         double elevation = asin((pt.z - FLAGS_lidar_height) / range) * 180 / M_PI;
 
         // keep in 0~360
-        if (azimuth < 0) {
+        if (azimuth < 0)
+        {
             azimuth += 360;
         }
 
         int x = int(azimuth / FLAGS_azimuth_resolution_deg);                      // 行
         int y = int((elevation + FLAGS_elevation_range) / ele_resolution + 0.5);  // 列
 
-        if (x >= 0 && x < image.cols && y >= 0 && y < image.rows) {
+        if (x >= 0 && x < image.cols && y >= 0 && y < image.rows)
+        {
             image.at<cv::Vec3b>(y, x) = cv::Vec3b(uchar(range / 100 * 255.0), 255, 127);
         }
     }
@@ -57,13 +61,15 @@ void GenerateRangeImage(PointCloudType::Ptr cloud) {
     cv::imwrite("./range_image.png", image_rgb);
 }
 
-int main(int argc, char** argv) {
+int main(int argc, char** argv)
+{
     google::InitGoogleLogging(argv[0]);
-    FLAGS_stderrthreshold = google::INFO;
+    FLAGS_stderrthreshold  = google::INFO;
     FLAGS_colorlogtostderr = true;
     google::ParseCommandLineFlags(&argc, &argv, true);
 
-    if (FLAGS_pcd_path.empty()) {
+    if (FLAGS_pcd_path.empty())
+    {
         LOG(ERROR) << "pcd path is empty";
         return -1;
     }
@@ -72,7 +78,8 @@ int main(int argc, char** argv) {
     PointCloudType::Ptr cloud(new PointCloudType);
     pcl::io::loadPCDFile(FLAGS_pcd_path, *cloud);
 
-    if (cloud->empty()) {
+    if (cloud->empty())
+    {
         LOG(ERROR) << "cannot load cloud file";
         return -1;
     }
