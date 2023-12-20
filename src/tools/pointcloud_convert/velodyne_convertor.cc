8e898c59eaee4e1ec9f591f0ecb14d871588295b
贝叶斯-成昌喜 <chengchangxi@holomatic.com>
2023-12-20 21:09:54 +0800

    Add: format

--- src/tools/pointcloud_convert/velodyne_convertor.cc
+++ src/tools/pointcloud_convert/velodyne_convertor.cc
@@ -6,11 +6,13 @@
 
 #include <glog/logging.h>
 
-namespace sad::tools {
-
-VelodyneConvertor::VelodyneConvertor(const VelodyneConfig &config) : velodyne_config_(config) {
+namespace sad::tools
+{
+VelodyneConvertor::VelodyneConvertor(const VelodyneConfig& config) : velodyne_config_(config)
+{
     packets_parser_ = std::make_shared<PacketsParser>();
-    if (packets_parser_->Setup(velodyne_config_) < 0) {
+    if (packets_parser_->Setup(velodyne_config_) < 0)
+    {
         LOG(ERROR) << "velodyne point cloud initialize raw data ERROR!!!";
         return;
     }
@@ -18,7 +20,8 @@ VelodyneConvertor::VelodyneConvertor(const VelodyneConfig &config) : velodyne_co
     converted_cloud_.reset(new FullPointCloudType());
 }
 
-void VelodyneConvertor::ProcessScan(const PacketsMsgPtr &packets_msg, FullCloudPtr &out_cloud) {
+void VelodyneConvertor::ProcessScan(const PacketsMsgPtr& packets_msg, FullCloudPtr& out_cloud)
+{
     packets_parser_->PaddingPointCloud(packets_msg, out_cloud);
 }
 
--- src/tools/pointcloud_convert/velodyne_convertor.h
+++ src/tools/pointcloud_convert/velodyne_convertor.h
@@ -5,13 +5,14 @@
 #include "tools/pointcloud_convert/packets_parser.h"
 #include "tools/pointcloud_convert/velodyne_config.h"
 
-namespace sad::tools {
-
+namespace sad::tools
+{
 /// velodyne输出的packets转换成pointcloud格式
 /// 实质上只是对packets_parser外面再包了一层
-class VelodyneConvertor {
-   public:
-    explicit VelodyneConvertor(const VelodyneConfig &config = VelodyneConfig());
+class VelodyneConvertor
+{
+public:
+    explicit VelodyneConvertor(const VelodyneConfig& config = VelodyneConfig());
 
     /**
      * 将packets msgs转换为FullCloud
@@ -19,12 +20,12 @@ class VelodyneConvertor {
      * @param packets_msg
      * @param out_cloud
      */
-    void ProcessScan(const PacketsMsgPtr &packets_msg, FullCloudPtr &out_cloud);
+    void ProcessScan(const PacketsMsgPtr& packets_msg, FullCloudPtr& out_cloud);
 
-   private:
-    VelodyneConfig velodyne_config_;
-    std::shared_ptr<PacketsParser> packets_parser_ = nullptr;
-    FullCloudPtr converted_cloud_ = nullptr;
+private:
+    VelodyneConfig                 velodyne_config_;
+    std::shared_ptr<PacketsParser> packets_parser_  = nullptr;
+    FullCloudPtr                   converted_cloud_ = nullptr;
 };
 
 }  // namespace sad::tools
